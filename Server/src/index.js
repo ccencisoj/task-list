e97ffefff2293d42e8cd78610852a95f12e2a2fb
cristian <bizapro123@gmail.com>
2022-06-27 18:38:00 -0500

    Se cambio el puerto, se elimino la configuracion de las vistas

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,26 +1,19 @@
 const express = require('express');
-const { join, dirname } = require('path');
-const { fileURLToPath } = require('url');
-const bodyParse = require('body-parser');
 const logger = require('morgan');
 const indexRouter = require('./routes/index');
+const port = process.env.PORT || 4000;
 
 const app = express();
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 //Middleware 
 app.use(logger('dev'));
-app.use(bodyParse.urlencoded({extended:false}));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
 
 //routers 
-
-app.use(indexRouter)
-
+app.use(indexRouter);
 
 //settings server
-app.set('port', process.env.PORT || 3000);
-app.set('view engine', 'ejs');
-app.set('views', join(__dirname, 'views'));
-app.listen(app.get('port'), () =>{
-    console.log('Puerto '+app.get('port')+' a la escucha');
+app.listen(port, ()=> {
+    console.log(`Server running on port ${port}`);
 });
