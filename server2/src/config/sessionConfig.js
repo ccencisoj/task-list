11a9b8748f9e95ab55595db9dbccff6a98e009d9
ccencisoj <97315510+ccencisoj@users.noreply.github.com>
2022-07-01 09:29:54 -0500

    Delete server2 directory

--- server2/src/config/sessionConfig.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  secret: process.env.SESSION_SECRET_KEY,
-  resave: false,
-  saveUninitialized: false
-};
--- server2/src/routes/index.js
+++
@@ -1,6 +0,0 @@
-const router = require("express").Router();
-
-router.use("/user", require("./user"));
-router.use("/task", require("./task"));
-
-module.exports = router;
