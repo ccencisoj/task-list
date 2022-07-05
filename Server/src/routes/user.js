313318ead9d63a61e4e11e730cfaf671b1225b24
cristian <bizapro123@gmail.com>
2022-07-05 11:34:21 -0500

    Cambios en la ruta user

--- Server/src/routes/user.js
+++ Server/src/routes/user.js
@@ -1,14 +1,19 @@
+const express = require("express");
 const { Router } = require('express');
 const path = require('path');
 const model = require('../model/user.js');
+
 const uploadFile = require('../middleware/multer');
 
 const router = Router();
+
+router.use("/user/image", express.static(path.resolve("./src/uploads")));
 
 router.get('/user', async(req, res)=>{
     let body = req.body;
     try {
         let user = await model.findOne(body);
+        req.session.user = user;
         res.json(user);
     } catch (error) {
         res.json(error);
@@ -48,11 +53,9 @@ router.put('/user', async (req, res)=>{
 })
 
 router.post('/user/upload', uploadFile(), (req, res)=>{
-    let image = req.files;
-    let documentName= req.file.filename;
-    let uri = path.resolve(__dirname, `../uploads/${documentName}`);
-    res.json({
-        avatar: uri
-    });
+    let image = req.file;
+    let uri = `${req.protocol}://${req.headers.host}/user/image/${image.filename}`;
+    res.json({avatar: uri});
 });
+
 module.exports = router;
--- client/.next/static/development/_buildManifest.js
+++ client/.next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/signUp":["static\u002Fchunks\u002Fpages\u002FsignUp.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002FsignUp"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 2793ec0eea3bf84a110d73e67cea34d7f668414d
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/signUp":["static\u002Fchunks\u002Fpages\u002FsignUp.js"],"/taskList":["static\u002Fchunks\u002Fpages\u002FtaskList.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002FsignUp","\u002FtaskList"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
